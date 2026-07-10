49af4e672b4c2605c39301883d493403354b67d5
Luka Crnkovic-Friis <luka@peltarion.com>
2026-07-10 17:52:09 +0200

    chore: release v0.7.27

--- sdks/typescript/src/generated/types.ts
+++ sdks/typescript/src/generated/types.ts
@@ -1,9 +1,9 @@
 // Generated wire types for Meerkat SDK
-// Contract version: 0.7.26
+// Contract version: 0.7.27
 
 import { MeerkatError } from "./errors.js";
 
-export const CONTRACT_VERSION = "0.7.26";
+export const CONTRACT_VERSION = "0.7.27";
 
 export type Value = unknown;
 
